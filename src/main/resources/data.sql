8a56f9f094349e1a22ca4af05866135e4e1b5365
briankimithi-sketch <briankimithi@gmail.com>
2026-06-30 10:30:34 +0300

    Initial commit - Spring Security demo with React frontend

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,2 +1,2 @@
-INSERT IGNORE INTO custom_user (username, password, role)
+INSERT IGNORE INTO CUSTOM_USER (username, password, `role`)
 VALUES ('user','$2a$12$4leyAR.ClA1VZo.k7800NObq8ZRd5L13/5CeotFoHCZ1UH1BqClsa', 'USER');
